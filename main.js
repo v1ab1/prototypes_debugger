ea71d80b7fd63567d57334ead71ed7f5737e4212
v1ab1 <sharkandmouse@gmail.com>
2023-04-16 19:51:35 +0500

    html protos

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
 const input = document.querySelector('.debugger_input');
 const button = document.querySelector('.debugger_submit');
-const button2 = document.querySelector('.debugger_submit2');
-
+const content = document.querySelector('.debugger_content');
 
 button.addEventListener('click', () => {
   const inputVal = input.value;
-  const constr = window[inputVal];
-  if (!constr) {
-    console.log(`Constructor ${inputVal} not found`);
-    return;
+  const JSed = window[inputVal];
+  const HTMLed = document.querySelector(input.value);
+  if (JSed) {
+    const proto = Object.getPrototypeOf(JSed);
+    // input.value = '';
+    console.log(proto);
+  } else if (HTMLed) {
+    showHTMLProtos(HTMLed);
+  } else {
+    console.log('not found');
   }
-  const proto = Object.getPrototypeOf(constr);
-  input.value = '';
-  console.log(proto);
 });
 
-button2.addEventListener('click', () => {
-  const inputValue = document.querySelector(input.value);
-  input.value = '';
-  console.log(inputValue.constructor);
-});
+const showHTMLProtos = (HTMLed) => {
+  let element = HTMLed.constructor;
+  while (true) {
+    try {
+      const div = document.createElement('div');
+      div.style.display = "flex";
+      div.style.flexDirection = "column";
+        const p = document.createElement('p');
+        p.innerHTML = typeof element === "function" ? element.name : element.constructor.name;
+        const button = document.createElement('button');
+        const elementToShow = element;
+        button.onclick = (() => {
+          console.log(elementToShow);
+          console.log(typeof elementToShow);
+        });
+        button.innerHTML = "console.log it";
+        button.style.width = "100px";
+        div.appendChild(p);
+        div.appendChild(button);
+      content.appendChild(div);
+      element = element.__proto__; 
+    } catch (error) {
+      return;
+    }
+  }
+};
