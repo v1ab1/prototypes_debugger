46d8906b4d3cec159043d5b19d5310b80a43aeda
v1ab1 <sharkandmouse@gmail.com>
2023-04-16 20:04:56 +0500

    predone

--- main.js
+++ main.js
@@ -3,19 +3,40 @@ const button = document.querySelector('.debugger_submit');
 const content = document.querySelector('.debugger_content');
 
 button.addEventListener('click', () => {
+  content.innerHTML = "";
   const inputVal = input.value;
   const JSed = window[inputVal];
   const HTMLed = document.querySelector(input.value);
   if (JSed) {
-    const proto = Object.getPrototypeOf(JSed);
-    // input.value = '';
-    console.log(proto);
+    showJSProtos(JSed);
   } else if (HTMLed) {
     showHTMLProtos(HTMLed);
   } else {
     console.log('not found');
   }
 });
+
+const showJSProtos = (JSed) => {
+  let proto = Object.getPrototypeOf(JSed);
+  while (proto) {
+    const div = document.createElement('div');
+    div.style.display = "flex";
+    div.style.flexDirection = "column";
+      const p = document.createElement('p');
+      p.innerHTML = proto.constructor.name;
+      const button = document.createElement('button');
+      const elementToShow = proto;
+      button.onclick = (() => {
+        console.log(elementToShow);
+      });
+      button.innerHTML = "console.log it";
+      button.style.width = "100px";
+      div.appendChild(p);
+      div.appendChild(button);
+    content.appendChild(div);
+    proto = Object.getPrototypeOf(proto); 
+  }
+};
 
 const showHTMLProtos = (HTMLed) => {
   let element = HTMLed.constructor;
@@ -30,14 +51,13 @@ const showHTMLProtos = (HTMLed) => {
         const elementToShow = element;
         button.onclick = (() => {
           console.log(elementToShow);
-          console.log(typeof elementToShow);
         });
         button.innerHTML = "console.log it";
         button.style.width = "100px";
         div.appendChild(p);
         div.appendChild(button);
       content.appendChild(div);
-      element = element.__proto__; 
+      element = Object.getPrototypeOf(element); 
     } catch (error) {
       return;
     }
