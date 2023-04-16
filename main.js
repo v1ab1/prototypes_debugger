b6a903da188001f4510e1dd2f858ed09de076eb7
v1ab1 <sharkandmouse@gmail.com>
2023-04-16 20:13:58 +0500

    start work on integration

--- main.js
+++ main.js
@@ -5,61 +5,32 @@ const content = document.querySelector('.debugger_content');
 button.addEventListener('click', () => {
   content.innerHTML = "";
   const inputVal = input.value;
-  const JSed = window[inputVal];
-  const HTMLed = document.querySelector(input.value);
-  if (JSed) {
-    showJSProtos(JSed);
-  } else if (HTMLed) {
-    showHTMLProtos(HTMLed);
+  const element = window[inputVal] || document.querySelector(inputVal);
+  if (element) {
+    showProtos(element);
   } else {
-    console.log('not found');
+    content.innerHTML = "Not found";
   }
 });
 
-const showJSProtos = (JSed) => {
-  let proto = Object.getPrototypeOf(JSed);
+const showProtos = (element) => {
+  let proto = Object.getPrototypeOf(element);
   while (proto) {
     const div = document.createElement('div');
     div.style.display = "flex";
     div.style.flexDirection = "column";
-      const p = document.createElement('p');
-      p.innerHTML = proto.constructor.name;
-      const button = document.createElement('button');
-      const elementToShow = proto;
-      button.onclick = (() => {
-        console.log(elementToShow);
-      });
-      button.innerHTML = "console.log it";
-      button.style.width = "100px";
-      div.appendChild(p);
-      div.appendChild(button);
+    const p = document.createElement('p');
+    p.innerHTML = typeof proto.constructor === "function" ? proto.constructor.name : proto.constructor.constructor.name;
+    const button = document.createElement('button');
+    const protoToShow = proto;
+    button.onclick = (() => {
+      console.log(protoToShow);
+    });
+    button.innerHTML = "console.log it";
+    button.style.width = "100px";
+    div.appendChild(p);
+    div.appendChild(button);
     content.appendChild(div);
     proto = Object.getPrototypeOf(proto); 
-  }
-};
-
-const showHTMLProtos = (HTMLed) => {
-  let element = HTMLed.constructor;
-  while (true) {
-    try {
-      const div = document.createElement('div');
-      div.style.display = "flex";
-      div.style.flexDirection = "column";
-        const p = document.createElement('p');
-        p.innerHTML = typeof element === "function" ? element.name : element.constructor.name;
-        const button = document.createElement('button');
-        const elementToShow = element;
-        button.onclick = (() => {
-          console.log(elementToShow);
-        });
-        button.innerHTML = "console.log it";
-        button.style.width = "100px";
-        div.appendChild(p);
-        div.appendChild(button);
-      content.appendChild(div);
-      element = Object.getPrototypeOf(element); 
-    } catch (error) {
-      return;
-    }
   }
 };
